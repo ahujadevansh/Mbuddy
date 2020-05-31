4e2348f6b552b6d1e3d7a9f44e133f34ecf92fd0
Devansh Ahuja <ahuja.devansh2@gmail.com>
2020-05-31 20:20:40 +0530

    Login functionality with google firebase

--- app/src/main/java/com/ahujadevansh/mbuddy/LoginActivity.kt
+++ app/src/main/java/com/ahujadevansh/mbuddy/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.ahujadevansh.mbuddy
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.ahujadevansh.mbuddy.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,5 +15,34 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.buttonSignIn.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+
+        val email = binding.emailEditTextSignIn.text.toString()
+        val password = binding.passwordEditTextSignIn.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill email and password fields", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isComplete) return@addOnCompleteListener
+                Toast.makeText(this, "hello ${it.result?.user?.email}", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Log.d("MAIN", "something went wrong")
+                Toast.makeText(
+                    this,
+                    "Please make sure your email and password is correct",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
--- app/src/main/java/com/ahujadevansh/mbuddy/MainActivity.kt
+++ app/src/main/java/com/ahujadevansh/mbuddy/MainActivity.kt
@@ -3,9 +3,11 @@ package com.ahujadevansh.mbuddy
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.ahujadevansh.mbuddy.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +18,10 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.signUpButtonRegister.setOnClickListener {
-            Log.d("MainActivity", binding.emailEditTextRegister.text.toString())
+
+            registerUser()
+
+
         }
 
         binding.alreadyHaveAnAccountTextViewRegister.setOnClickListener {
@@ -24,5 +29,29 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun registerUser() {
+        val email = binding.emailEditTextRegister.text.toString()
+        val password = binding.passwordEditTextRegister.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill email and password fields", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isComplete) return@addOnCompleteListener
+                Toast.makeText(this, "hello ${it.result?.user?.email}", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Log.d("MAIN", "something went wrong")
+                Toast.makeText(
+                    this,
+                    "Please make sure your email and password is correct",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
